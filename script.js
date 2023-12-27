f4920068591e577ef9d4dec43ae35a9666fd1b43
Shivam Singh gautam <105829038+Shivu150199@users.noreply.github.com>
2023-12-28 00:05:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 //your code here!
 
+
+let container = document.querySelector('#infi-list')
+let count=0
+for(;count<=10;count++){
+  let div =document.createElement('li')
+  div.innerText = `item ${count}`
+  container.appendChild(div)
+}
+
+container.addEventListener('scroll',()=>{
+  let scrolledHeight=container.scrollTop
+  console.log(container.scrollHeight)
+  console.log(container.clientHeight)
+  let maxScrollHeight = container.scrollHeight - container.clientHeight
+  if(maxScrollHeight-scrolledHeight<=5){
+    console.log('hello')
+  addTwo()
+  }
+})
+
+
+
+function addTwo(){
+  let div1 = document.createElement('li')
+  div1.innerText = `item ${count++}`
+  container.appendChild(div1)
+  let div2 = document.createElement('li')
+  div2.innerText = `item ${count++}`
+  container.appendChild(div2)
+}
